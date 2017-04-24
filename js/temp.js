4f6dda6982677a36cf9f971b9431ab94d09b994a
Tanya Ramirez <tcramire@gmail.com>
2017-04-24 17:35:00 -0300

    Alert funciona ok finalmente

---
+++ js/temp.js
@@ -0,0 +1,4 @@
+function convertirCelsius(temp){
+    var temp = document.getElementById('celsius').value//getElementById une con el boton y lo hace funcionar
+    alert(((temp*9/5) + 32) + "°F");
+}
