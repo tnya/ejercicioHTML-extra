689d730a9f1472fa18359e8665b8ebbb8433afd2
Tanya Ramirez <tcramire@gmail.com>
2017-04-24 20:26:09 -0300

    ahora funciona la formula pero no pude aplicar math.round (?)

--- js/temp.js
+++ js/temp.js
@@ -1,4 +1,4 @@
 function convertirCelsius(temp){
-    var temp = document.getElementById('celsius').value//getElementById une con el boton y lo hace funcionar
-    alert(((temp*9/5) + 32) + "°F");
+    var temp = document.getElementById('farenheit').value//getElementById une con el boton y lo hace funcionar
+    alert(((temp-32)*5/9) + "°C");
 }
